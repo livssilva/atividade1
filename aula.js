23681fbd70f8d701ce5fc355106eb59570139e00
Lívia Azevedo <liviaazevedostz@gmail.com>
2025-02-20 11:34:50 -0300

    Atividade 1

---
+++ aula.js
@@ -0,0 +1,9 @@
+console.clear();
+var saudacao = "Ol\u00E1!!";
+var nome = "L\u00EDvia";
+var idade = 15;
+var div1 = "assistir s\u00E9rie";
+var div2 = "ajudar nas tarefas de casa";
+var div3 = "ouvir m\u00FAsica";
+var formacao = "biologia";
+console.log("".concat(saudacao, " me chamo ").concat(nome, ", tenho ").concat(idade, " anos no meu tempo livre gosto de ").concat(div1, ", ").concat(div2, " e ").concat(div3, ".\n Tenho a inten\u00E7\u00E3o de me formar em ").concat(formacao));
---
+++ aula.ts
@@ -0,0 +1,12 @@
+console.clear();
+let saudacao: string = `Olá!!`;
+let nome: string = `Lívia`;
+let idade: number = 15;
+let div1: string = `assistir série`;
+let div2: string = `ajudar nas tarefas de casa`;
+let div3: string = `ouvir música`;
+let formacao: string = `biologia`;
+
+console.log (`${saudacao} me chamo ${nome}, tenho ${idade} anos no meu tempo livre gosto de ${div1}, ${div2} e ${div3}.
+ Tenho a intenção de me formar em ${formacao}`)
+ 
